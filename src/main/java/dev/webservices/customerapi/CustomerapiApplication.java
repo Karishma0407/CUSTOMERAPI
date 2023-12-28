24707f3e17608b8ba43e32df3e316ddf30668e09
Karishma0407 <karishma0407@icloud.com>
2023-12-28 14:30:53 -0500

    Updated CustomerapiApplication.java

--- src/main/java/dev/webservices/customerapi/CustomerapiApplication.java
+++ src/main/java/dev/webservices/customerapi/CustomerapiApplication.java
@@ -1,9 +1,11 @@
 package dev.webservices.customerapi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class CustomerapiApplication {
 
 	public static void main(String[] args) {
