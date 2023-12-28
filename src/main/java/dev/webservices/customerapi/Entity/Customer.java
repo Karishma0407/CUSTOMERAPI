c9136e5426ec1bd69c9247dde5ac02980c757d43
Karishma0407 <karishma0407@icloud.com>
2023-12-28 14:00:15 -0500

    Initial commit

---
+++ src/main/java/dev/webservices/customerapi/Entity/Customer.java
@@ -0,0 +1,9 @@
+package dev.webservices.customerapi.Entity;
+
+public class Customer {
+
+    // private Long id;
+    // private String firstName;
+    // private String lastName;
+    // private String email;
+}
