ebb2019f38fb38839f9bd18e5b83013ebd703ca9
Karishma0407 <karishma0407@icloud.com>
2023-12-29 10:42:51 -0500

    Impl findById

--- src/main/java/dev/webservices/customerapi/Controller/CustomerController.java
+++ src/main/java/dev/webservices/customerapi/Controller/CustomerController.java
@@ -1,9 +1,11 @@
 package dev.webservices.customerapi.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.webservices.customerapi.Entity.Customer;
@@ -23,5 +25,11 @@ public class CustomerController {
         customerService.save(customer);
 
         System.out.println("Customer data saved!");
+    }
+
+    // Find customer using Id
+    @GetMapping("/find")
+    public Customer getCustomer(@RequestParam Long id) {
+        return customerService.findById(id).get();
     }
 }
